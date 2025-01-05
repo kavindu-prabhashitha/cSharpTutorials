336194a6439b8858021e814672368c323197799b
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2025-01-05 20:30:46 +0530

    exception Programs added

---
+++ ExceptionProgramms/CustomException.cs
@@ -0,0 +1,14 @@
+namespace ExceptionProgramms;
+
+public class CustomException:Exception
+{
+    public CustomException(string message):base(message)
+    {
+        
+    }
+
+    public CustomException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
+}
---
+++ ExceptionProgramms/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ExceptionProgramms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //throw new Exception();
+                throw new CustomException("This is a custom exception");
+            }
+            catch (CustomException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+    }
+}
+
